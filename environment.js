53633bc4fbc8484a5d7a9189d0112d6eb49180a6
HoaPham <phamhuyhoa03@gmail.com>
2024-12-27 05:30:46 +0700

    Final version

--- environment.js
+++ environment.js
@@ -1,2 +1,2 @@
-export const root_domain = '192.168.201.104:3000';
-export const root_python = '192.168.201.104:5000';
+export const root_domain = '192.168.85.239:3000';
+export const root_python = '192.168.85.239:5000';
